9401eb50661193b3ca7606667cceeca601edd23f
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2022-02-07 18:49:29 +0300

    adv task

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''places blueprint'''
 
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
-from models import storage
+from models import storage, storage_t
 from models.place import Place
+from models.state import State
 from models.city import City
 from models.user import User
 
@@ -83,6 +84,85 @@ def postPlace(city_id):
     place = Place(**body)
     place.save()
     return make_response(jsonify(place.to_dict()), 201)
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def placesSearch():
+    '''retrives all place objects depending on the request body'''
+    body = request.get_json()
+    if body is None:
+        abort(400, 'Not a JSON')
+    keys = body.keys()
+    if len(body) <= 0\
+       or (('states' not in keys or len(body['states']) <= 0) and
+       ('cities' not in keys or len(body['cities']) <= 0)):
+        if 'amenities' not in keys:
+            places = storage.all(Place).values()
+            dcts = [pl.to_dict() for pl in places]
+            if storage_t == 'db':
+                for idx in range(len(dcts)):
+                    if 'amenities' in dcts[idx].keys():
+                        del dcts[idx]['amenities']
+            return jsonify(dcts)
+        else:
+            places = storage.all(Place).values()
+            unwanted = []
+            for idx, place in enumerate(places):
+                if storage_t == 'db':
+                    amens_ids = [m.id for m in place.amenities]
+                else:
+                    amens_ids = place.amenity_ids
+                for amenity_id in body['amenities']:
+                    if amenity_id not in amens_ids:
+                        unwanted.append(idx)
+                        break
+            for idx, i in enumerate(unwanted):
+                del places[i - idx]
+            dcts = [pl.to_dict() for pl in places]
+            if storage_t == 'db':
+                for idx in range(len(dcts)):
+                    if 'amenities' in dcts[idx].keys():
+                        del dcts[idx]['amenities']
+            return jsonify(dcts)
+
+    places = storage.all(Place).values()
+    wanted_places = []
+    cities = {}
+
+    if 'cities' in keys:
+        for cityId in body['cities']:
+            cities[cityId] = cityId
+
+    if 'states' in keys:
+        for state in storage.all(State).values():
+            if state.id in body['states']:
+                for city in state.cities:
+                    cities[city.id] = city.id
+
+    for place in places:
+        if len(cities) > 0:
+            if place.city_id in cities:
+                wanted_places.append(place)
+    unwanted = []
+    if 'amenities' in keys:
+        for idx, place in enumerate(wanted_places):
+            if storage_t == 'db':
+                amens_ids = [m.id for m in place.amenities]
+            else:
+                amens_ids = place.amenity_ids
+            for amenity_id in body['amenities']:
+                if amenity_id not in amens_ids:
+                    unwanted.append(idx)
+                    break
+
+    for idx, i in enumerate(unwanted):
+        del wanted_places[i - idx]
+    dcts = [pl.to_dict() for pl in wanted_places]
+    if storage_t == 'db':
+        for idx in range(len(dcts)):
+            if 'amenities' in dcts[idx].keys():
+                del dcts[idx]['amenities']
+    return jsonify(dcts)
 
 
 @app_views.route('/places/<place_id>',
